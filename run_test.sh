940683d7f76c4864fa160373fc418bf19a0ad67e
Jonathan <frik_ej2v@hotmail.com>
2020-12-14 00:03:34 -0400

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+virtualenv rsvpapp --system-site-packages -v
+source rsvpapp/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 python -m pytest tests/test_rsvpapp.py
